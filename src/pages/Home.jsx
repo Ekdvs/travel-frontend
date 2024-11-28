865dd7c7d912768221851d528ba49f25014cad2e
Ekdvs <ekdvsampath02@gmail.com>
2024-11-28 11:59:44 +0530

    add nav bar to home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Topbar from '../components/Topbar'
+import Navbar from '../components/Navbar'
 
 export default function Home() {
   return (
     <div>
-      <Topbar></Topbar>
+      <Topbar/>
+      <Navbar/>
+
+      
     </div>
   )
 }
