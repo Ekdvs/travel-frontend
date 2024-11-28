e27558f8ed024be440562ba2377089b68aeb202f
Ekdvs <ekdvsampath02@gmail.com>
2024-11-28 12:16:29 +0530

    n

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 
 import './App.css';
+import Home from './pages/Home';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <div>
-      <div></div>
-      <div></div><div></div><div></div>
+     <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+         
+
+
+
+          
+        </Routes>
+      </Router>
       
     </div>
   );
