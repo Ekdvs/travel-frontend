155ad5becec925bf27467f520a93c907b607ffd6
Ekdvs <ekdvsampath02@gmail.com>
2024-11-28 12:00:32 +0530

    add footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Topbar from '../components/Topbar'
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 
 export default function Home() {
   return (
     <div>
       <Topbar/>
       <Navbar/>
+    
+      <Footer/>
 
       
     </div>
